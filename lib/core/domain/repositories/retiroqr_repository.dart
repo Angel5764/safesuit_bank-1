324aefe293fb4b745fcf045440b74a417afb59ce
Angel <angel5764.zs@gmail.com>
2024-06-04 00:31:57 -0600

    Usecase RetiroQR

--- lib/core/domain/repositories/retiroqr_repository.dart
+++ lib/core/domain/repositories/retiroqr_repository.dart
@@ -1,5 +1,5 @@
 import 'package:safesuit_bank/core/domain/models/retiroqrModel.dart';
 
 abstract class RetiroqrRepository{
-  Future<RetiroqrModel> loadFormData();
+  Future<RetiroqrModel> loadRetiroData();
 }
