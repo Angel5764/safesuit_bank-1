5a6f2a885a1e427df7793ea473f415ee415e95d1
MrPegasus194 <formal_phoenix2000@hotmail.com>
2024-06-05 11:05:26 -0500

    Cambios

--- lib/core/domain/usecases/load_sidebar_data.dart
+++ lib/core/domain/usecases/load_sidebar_data.dart
@@ -1,10 +1,43 @@
 import 'package:safesuit_bank/core/domain/models/sidebarModel.dart';
+import 'package:safesuit_bank/core/domain/repositories/sidebar_repository.dart';
 
 class LoadSidebarData {
+  final SidebarRepository repository;
+
+  LoadSidebarData(this.repository);
+
   Future<SidebarModel> call() async {
-    return SidebarModel(
-      username: 'Angel Zea',
-      email: 'angelzea2024@gmail.com'
-    );
+    final sidebarData = await repository.loadFormData();
+
+    // Validaciones
+    if (sidebarData.username.isEmpty) {
+      throw Exception("El nombre de usuario no puede estar vacío");
+    }
+    if (sidebarData.username.length < 3) {
+      throw Exception("El nombre de usuario debe tener al menos 3 caracteres");
+    }
+    if (!_esCarateres(sidebarData.username)) {
+      throw Exception("El nombre de usuario solo puede contener letras");
+    }
+    if (sidebarData.email.isEmpty) {
+      throw Exception("El correo electrónico no puede estar vacío");
+    }
+    if (!_esEmailValido(sidebarData.email)) {
+      throw Exception("El formato del correo electrónico no es válido");
+    }
+
+    return sidebarData;
+  }
+
+//Verifica si una cadena contiene solo letras (caracteres alfabéticos).
+  bool _esCarateres(String str) {
+    final alphaRegex = RegExp(r'^[a-zA-Z]+$');
+    return alphaRegex.hasMatch(str);
+  }
+
+//Verifica si una cadena tiene un formato de correo electrónico válido.
+  bool _esEmailValido(String email) {
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+$');
+    return emailRegex.hasMatch(email);
   }
 }
