20b78fe3751ce29eac711f4d107c08d1be2c64ef
Brandon <brandon.lopes.diaz@gmail.com>
2024-07-24 12:33:51 -0500

    pago de servicios aguakan

--- lib/data/repositories/pagaraguakan_repository_impl.dart
+++ lib/data/repositories/pagaraguakan_repository_impl.dart
@@ -5,43 +5,46 @@ import 'package:safesuit_bank/core/domain/repositories/pagaraguakan_repository.d
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class PagaraguakanRepositoryImpl implements PagaraguakanRepository{
-    final Dio _dio = Dio();
+class PagaraguakanRepositoryImpl implements PagaraguakanRepository {
+  final Dio _dio = Dio();
 
   @override
-  Future<pagaraguakanModel> loadFormData({required String NIA,   required double Importe}) async {
+  Future<pagaraguakanModel> loadFormData(
+      {required String NIA, required double Importe}) async {
     final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('auth_token');  
-     final cuentaresponse = await _dio.get(
-        'https://apimoviles-production.up.railway.app/accounts/me',
-        options: Options(
-          headers: {
-            'Authorization': 'Bearer $token',
-          },
-        ),
-      );
-    print(cuentaresponse.data);
-    final data = cuentaresponse.data;
-    final int id = data['id'];
-    final int idUser = data['id_user'];
-     
-    final response = await _dio.post(
-        'https://apimoviles-production.up.railway.app/log-services',
-        data: {
-          'id_service': "11",
-          'id_users': idUser,
-          'id_account': id,
-          'amount': Importe,
-          'reference': NIA
+    final token = prefs.getString('auth_token');
+    final cuentaresponse = await _dio.get(
+      'https://apimoviles-production.up.railway.app/accounts/me',
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $token',
         },
-        options: Options(
-          headers: {
-            'Authorization': 'Bearer $token',
-          },
-        ),
-      );
-      print(response);
-    
+      ),
+    );
+    print(cuentaresponse.data);
+    final info = cuentaresponse.data["data"];
+    final id = info["id"];
+    final idUser = info["id_user"];
+
+    print("id: $id, idUser: $idUser");
+
+    final response = await _dio.post(
+      'https://apimoviles-production.up.railway.app/log-services',
+      data: {
+        'id_service': "11",
+        'id_users': idUser,
+        'id_account': id,
+        'amount': Importe,
+        'reference': NIA
+      },
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      ),
+    );
+    print(response.data);
+
     return pagaraguakanModel.fromJson(response.data);
   }
 }
